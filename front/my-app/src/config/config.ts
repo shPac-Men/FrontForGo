72c5340f0d0e246531e1a44ed1f337be6e1004a4
shPac-Men <dlya-igor-artem@yandex.ru>
2025-12-12 06:30:30 +0300

    githubPages2

--- front/my-app/src/config/config.ts
+++ front/my-app/src/config/config.ts
@@ -1,5 +1,13 @@
-//тут держим ссылки для гитхаб паджес
-export const STATIC_BASE = import.meta.env.VITE_STATIC_BASE ?? '/staticimages';
+// //тут держим ссылки для гитхаб паджес
+// export const STATIC_BASE = import.meta.env.VITE_STATIC_BASE ?? '/staticimages';
+
+// export const API_BASE = import.meta.env.VITE_API_BASE ?? '/api/v1';
+
+// src/config/config.ts
+const isGithubPages = location.hostname === 'shpac-men.github.io';
+
+export const STATIC_BASE = isGithubPages
+  ? '/FrontendElements/staticimages'
+  : '/staticimages';
 
 export const API_BASE = import.meta.env.VITE_API_BASE ?? '/api/v1';
-
